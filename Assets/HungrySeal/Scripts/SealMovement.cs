43fe07eb15391cd041b1b8d185ac00f1de0d72bf
MoNireu <monireu.dev@gmail.com>
2020-09-14 21:58:00 +0900

    feat: 대쉬 기능추가. 횡 이동량 변경.

--- Assets/HungrySeal/Scripts/SealMovement.cs
+++ Assets/HungrySeal/Scripts/SealMovement.cs
@@ -6,101 +6,135 @@ public class SealMovement : MonoBehaviour
 {
 
     public Rigidbody playerRd;
-    public Transform playerTf;
+    public Transform groundCheck;
+    public LayerMask groundMask;
 
-    private float upForce = 3000f;
-    private float downForce = -1000f;
-    private float rightForce = 2500f;
-    private float leftForce = -2500f;
+    [SerializeField]
+    private float originalHorizontalForce = 2500f;
+    [SerializeField]
+    private float originalVerticalForce = 3000f;
+
+    private float horizontalForce;
+    private float verticalForce;
 
 
     private bool isWater;
-    private int jumpCnt = 0;
+    private bool isGround;
 
     private float horizontalRotate = -80f;
     private float verticalRotate = 0f;
 
 
+    // gravity
+    [SerializeField]
+    public float groundGravity = -15f;
+    [SerializeField]
+    public float oceanGravity = -1f;
+    [SerializeField]
+    public float groundCheckRadius = 0.4f;
+
+
     //// Start is called before the first frame update
     void Start()
     {
+        horizontalForce = originalHorizontalForce;
+        verticalForce = originalVerticalForce;
     }
 
     //// Update is called once per frame
     void Update()
     {
+        isGround = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+
+
+        // 공중시 횡입력 감소
+        if (!isWater && !isGround)
+        {
+            horizontalForce = 1000f;
+        }
+        else
+        {
+            horizontalForce = originalHorizontalForce;
+        }
+
+        // Input Controll
+
+        //dash
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            horizontalForce = 20000f;
+            verticalForce = 20000f;
+        }else
+        {
+            horizontalForce = originalHorizontalForce;
+            verticalForce = originalVerticalForce;
+        }
+
+
+        float InputX = Input.GetAxis("Horizontal");
+        float InputY = Input.GetAxis("Vertical");
 
 
         if (Input.GetKey("w")) //up
         {
             if (isWater)
             {
-                playerRd.AddForce(0, upForce * Time.deltaTime, 0);
+                playerRd.AddForce(0, verticalForce * Time.deltaTime, 0);
                 verticalRotate -= 4f;
-                verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);                
-                playerTf.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
+                verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);
+                transform.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
             }
+            else
+            {
+                centerPitch();
+            }            
         }
 
         if (Input.GetKey("s")) //down
         {
-            playerRd.AddForce(0, downForce * Time.deltaTime, 0);
-            verticalRotate += 4f;
-            verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);            
-            playerTf.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
-
-        }
-
-        if (Input.GetKey("a")) //left
-        {
-            playerRd.AddForce(leftForce * Time.deltaTime, 0, 0);
-            horizontalRotate -= 10f;
-            horizontalRotate = Mathf.Clamp(horizontalRotate, -90f, 90f);            
-            playerTf.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
-
-        }
-
-        if (Input.GetKey("d")) // right
-        {
-            playerRd.AddForce(rightForce * Time.deltaTime, 0, 0);
-            horizontalRotate += 10f;
-            horizontalRotate = Mathf.Clamp(horizontalRotate, -90f, 90f);            
-            playerTf.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
-
-        }
-
-
-        if (!Input.GetKey("w") && !Input.GetKey("s"))
-        {
-            if (verticalRotate > 0)
+            if (isWater)
             {
-                verticalRotate -= 1f;
+                playerRd.AddForce(0, -verticalForce * Time.deltaTime, 0);
+                verticalRotate += 4f;
                 verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);
-                playerTf.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
-            }
-            else if (verticalRotate < 0)
-            {
-                verticalRotate += 1f;
-                verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);
-                playerTf.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
+                transform.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
             }
             else
             {
-                playerTf.rotation = Quaternion.Euler(0, horizontalRotate, 0);
-            }
+                centerPitch();
+            }           
         }
 
+        if (Input.GetKey("a")) //left
+        {            
+            playerRd.AddForce(-horizontalForce * Time.deltaTime, 0, 0);
+            horizontalRotate -= 10f;
+            horizontalRotate = Mathf.Clamp(horizontalRotate, -90f, 90f);
+            transform.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
+        }
 
+        if (Input.GetKey("d")) // right
+        {            
+            playerRd.AddForce(horizontalForce * Time.deltaTime, 0, 0);
+            horizontalRotate += 10f;
+            horizontalRotate = Mathf.Clamp(horizontalRotate, -90f, 90f);
+            transform.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
+        }        
 
+        // centerPitch
+        if (!Input.GetKey("w") && !Input.GetKey("s"))
+        {
+            centerPitch();
+        }
 
-
-
+   
     }  
 
     private void OnTriggerEnter(Collider other)
     {
         isWater = true;
         jumpCnt = 1;
+        Physics.gravity = new Vector3(0, oceanGravity, 0);
     }
 
     private void OnTriggerExit(Collider other)
@@ -109,13 +143,29 @@ public class SealMovement : MonoBehaviour
         jump();
         jumpCnt -= 1;
         Debug.Log("notWater");
+        Physics.gravity = new Vector3(0, groundGravity, 0);
     }
 
-    private void jump()
+    private void centerPitch()
     {
-        if (jumpCnt == 1)
+        float verticalRotateRate = 2f;
+        if (verticalRotate > 0)
         {
-            playerRd.AddForce(0, 500f, 0);
+            verticalRotate -= verticalRotateRate;
+            verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);
+            transform.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
         }
-    }
+        else if (verticalRotate < 0)
+        {
+            verticalRotate += verticalRotateRate;
+            verticalRotate = Mathf.Clamp(verticalRotate, -60f, 60f);
+            transform.rotation = Quaternion.Euler(verticalRotate, horizontalRotate, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, horizontalRotate, 0);
+        }
+    } 
+
+    
 }
---
+++ Assets/HungrySeal/Scripts/Water.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public static bool isWater = false;
+
+    [SerializeField] private float waterDrag;
+    private float originDrag;
+
+    [SerializeField] private Color waterColor;
+    [SerializeField] private float waterFogDensity;
+    [SerializeField] private float waterGravity;
+
+
+    private Color originColor;
+    private float originFogDensity;
+    private float originGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originColor = RenderSettings.fogColor;
+        originFogDensity = RenderSettings.fogDensity;
+        originGravity = -15f;
+
+        originDrag = 1;
+
+        Physics.gravity = new Vector3(0, originGravity, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            GetInWater(other);
+            Debug.Log("Inside Water!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            GetOutWater(other);
+            Debug.Log("Outside Water!");
+        }
+    }
+
+
+    private void GetInWater(Collider _player)
+    {
+        isWater = true;
+        _player.transform.GetComponent<Rigidbody>().drag = waterDrag;
+
+        RenderSettings.fogColor = waterColor;
+        RenderSettings.fogDensity = waterFogDensity;
+        Physics.gravity = new Vector3(0, waterGravity, 0);
+    }
+
+    private void GetOutWater(Collider _player)
+    {
+        isWater = false;
+        _player.transform.GetComponent<Rigidbody>().drag = originDrag;
+
+        RenderSettings.fogColor = originColor;
+        RenderSettings.fogDensity = originFogDensity;
+        Physics.gravity = new Vector3(0, originGravity, 0);
+    }
+}
